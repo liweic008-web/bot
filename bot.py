b529111186f9dbadbae05784da599d26890b6dc8
liweic008-web <liweic008@gmail.com>
2026-08-14 01:59:02 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -124,7 +124,7 @@ def get_buddhist_quote():
     report = f" ═══ 【星雲法師．雲端晨間開示】 ═══ \n"
     report += f" 雲端曆法：{now.strftime('%Y/%m/%d %H:%M')}\n\n"
     report += f"✨ 『 {quote} 』 ✨\n\n"
-    report += f"願各位太空居士，今日心無罣礙，事事順心。"
+    report += f"願各位咚大學子，今日心無罣礙，事事順心。"
     return report
 
 @bot.event
